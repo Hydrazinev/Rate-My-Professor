9968ebda73cffecfe5912da635a04464f32c3073
Vaidik Shah <87151310+Hydrazinev@users.noreply.github.com>
2025-09-05 22:21:35 -0700

    Remove vercel.json; rely on default routing

--- api/index.py
+++ api/index.py
@@ -1,3 +1,3 @@
 ﻿import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # add repo root to import main.py
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # add repo root
 from main import app
